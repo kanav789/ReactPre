4162a3bf49498e6a998d490ea98de25eecea1d1f
kanav789 <kanavrana91@gmail.com>
2025-02-18 15:35:09 +0530

    create a selctfilterpage

---
+++ FilterSelct/src/App.tsx
@@ -0,0 +1,21 @@
+
+
+import FilterCom from "./FilterCom";
+function App() {
+ 
+  const arrayofSelect =[
+    "Amazone","Woo Eccommerce","shopify","Magneto","Wix","Opencart","Zoho"]
+
+ 
+
+
+  return (
+ 
+
+    <div> <FilterCom  Options={arrayofSelect} /></div>
+
+     
+  )
+}
+
+export default App
